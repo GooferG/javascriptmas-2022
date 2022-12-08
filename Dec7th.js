aba61a6e7e726416933e3614db3d134c0762a8fc
Luiz Meneghim <luiz.meneghim@hotmail.com>
2022-12-07 21:47:51 -0700

    finished challenge

--- Dec7th.js
+++ Dec7th.js
@@ -1,13 +1,12 @@
 /* Alternating Caps 
- Write a function that takes in a string of letters
- and returns a sentence in which every other letter is capitalized.
+Write a function that takes in a string of letters
+and returns a sentence in which every other letter is capitalized.
 
 Example input: "I'm so happy it's Monday"
 Example output: "I'M So hApPy iT'S MoNdAy"
 */
 
 function altCaps(str) {
-  const newStr = '';
   return str
     .split('')
     .map((l, i) => (i % 2 === 0 ? l.toUpperCase() : l.toLowerCase()))
