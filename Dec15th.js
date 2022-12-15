8ab7bb00d2f3d7645fefa16fc0c45c2db1ce3bc9
Luiz Meneghim <luiz.meneghim@hotmail.com>
2022-12-14 21:38:22 -0700

    finished with test

--- Dec15th.js
+++ Dec15th.js
@@ -6,13 +6,15 @@ Write a function to check if a lowercased string of letters is a palindrome.
 If the word is palidrome, return true. If it isn't, return false. 
 */
 
-function isPalindrome(str){
-	return str.split('').reverse().join('') === str
+function isPalindrome(str) {
+  return str.split('').reverse().join('') === str;
 }
 
 // Test your function
-console.log(isPalindrome("abba"));
-console.log(isPalindrome("civic"));
-console.log(isPalindrome("octopus"));
-console.log(isPalindrome("pumpkins"));
-console.log(isPalindrome("madam"));
+console.log(isPalindrome('abba'));
+console.log(isPalindrome('civic'));
+console.log(isPalindrome('octopus'));
+console.log(isPalindrome('pumpkins'));
+console.log(isPalindrome('madam'));
+
+//testing git command
