1c862b81c4de6b4104e922768018d7e18a7456cb
Luiz Meneghim <luiz.meneghim@hotmail.com>
2022-12-20 22:37:50 -0700

    finished challenge

--- Dec21st.js
+++ Dec21st.js
@@ -19,6 +19,18 @@ Your function's output should look something like this:
 5 - $100,000 bonus!
  */
 
-function awardBonuses() {}
+function awardBonuses() {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('JACKPOT! 1 Million and a Yacht!');
+    } else if (i % 3 === 0) {
+      console.log('Vacation!');
+    } else if (i % 5 === 0) {
+      console.log('$100,000 bonus!');
+    } else {
+      console.log(':(');
+    }
+  }
+}
 
 awardBonuses();
